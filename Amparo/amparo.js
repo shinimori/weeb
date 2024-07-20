9dcecc374a76ea4fa0666e88863ea82b31e6bcbd
shinimori <andrea.ldu9@gmail.com>
2024-07-19 20:13:22 -0500

    Add files via upload

---
+++ Amparo/amparo.js
@@ -0,0 +1,32 @@
+ponerFoto=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoUno");
+    cmpImagen.src="amparo4.jpg";
+    alert("Eres mi apoyo constante y mi compañera de risas, por eso no dudes cunato te quiero.");
+}
+
+ponerFotoDos=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoDos");
+    cmpImagen.src="amparo3.jpg";
+}
+ponerFotoTres=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoTres");
+    cmpImagen.src="amparo2.jpg";
+}
+ponerFotoCuatro=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoCuatro");
+    cmpImagen.src="amparo5.jpg";
+}
+ponerFotoCinco=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoCinco");
+    cmpImagen.src="amparo6.jpg";
+}
+ponerFotoSeis=function(){
+    let cmpImagen;
+    cmpImagen=document.getElementById("fotoSeis");
+    cmpImagen.src="leiVideo.mp4";
+}
